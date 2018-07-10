f1eb70dc9f100a3963a86cd17ab1cfc6f9a55fd7
anselrd <anro6767@colorado.edu>
2018-07-09 22:29:18 -0700

    Changes logic of solvepuzzle.m to a simpler format
    Now rather than going through and tediously trying to replicate my own solution techniques, this simply removes
    from the pencil array of each box the numbers in the row and column and the 'sub-box' (the three-by-three) that
    the cell is in. This doesn't work as far as solving the puzzle goes but it does run so I am committing it. The
    next commit will add the further required logic that may result in a working solver.

--- solvepuzzle.m
+++ solvepuzzle.m
@@ -1,8 +1,9 @@
 function [ solved ] = solvepuzzle( clues )
+%todo: write a description here
 
-% initialize board
-pen = clues; % answers we know
-pencil = cell(9); % options for each other square
+% initialize board with given clues
+pen = clues;
+pencil = cell(9);
 for k = 1:81
     if pen(k) == 0
         pencil{k} = 1:9;
@@ -10,62 +11,69 @@ for k = 1:81
         pencil{k} = pen(k);
     end
 end
-lengths = clues./clues; 
-lengths(isnan(lengths)) = 9; % number of options for each square??
-r = mod(sum(lengths,2),9); % number in each row that are known??
-c = mod(sum(lengths,1),9); % number in each column that are known??
+% determine how many knowns are in each row and column and how many options
+% there are for each square on the board
+lengths = clues./clues;
+lengths(isnan(lengths)) = 9;
 
-% solved flag
-solved = 0;
+% set the sub-box bounds
+sub = [1 1 1 4 4 4 7 7 7; 3 3 3 6 6 6 9 9 9]';
 
-% sum of 1-9
-S = sum(1:9); % cool way of writing 45 lol
-
-while ~solved
-    % look at every element
-    for k = 1:9 % k = ROWS
-        for j = 1:9 % j = COLUMNS
-            if lengths(k,j) == 1 
-                % if this entry has been solved
-                if pen(k,j) == 0 
-                    % if we haven't written the solution, write it
+% iterate until solved
+while 1
+    
+    % look at every element that hasn't been solved yet, starting by
+    % looping through rows, then columns
+    for k = 1:9
+        for j = 1:9
+            
+            % ignore solved entries
+            if pen(k,j) == 0
+                
+                if lengths(k,j) == 1
+                    
+                    % if process of elimination has identified the
+                    % solutions for this box, write it in
                     pen(k,j) = pencil{k,j}(1);
+                    
                 else
-                    % otherwise leave it alone
-                    continue
-                end
-            else
-                if r(k) == 8
-                    % if this is the last element in its row, fill it in
-                    lengths(k,j) = 1;
-                    pen(k,j) = S - sum(pen(k,:));
-                    r(k) = 9;
-                elseif c(j) == 8
-                    % if this is the last element in its column, fill it in
-                    lengths(k,j) = 1;
-                    pen(k,j) = S - sum(pen(:,j));
-                    c(j) = 9;
-                elseif lengths(k,j) + length(unique([pen(k,:) pen(:,j)'])) ~= 9
-                    % if we haven't used this element's column and row to
-                    % eliminate possibilities, do that
+                    
+                    % get the known entries in the row, column, and sub-box
+                    % corresponding to the current element
+                    row = pen(k,:)';
+                    column = pen(:,j);
+                    sub_box = pen(sub(k,1):sub(k,2),sub(j,1):sub(j,2));
+                    
+                    % the intersection of these three can be eliminated
+                    % from the pencil list of this element
+                    eliminations = unique([row; column; sub_box(:)]);
+                    
+                    % loop through the pencil list removing any list
+                    % entries that match an entry in 'eliminations'
                     removed = 0;
                     for n = 1:lengths(k,j)
-                        % if a pencil mark appears in row k or column j for
-                        % this element, erase it
-                        if any(pen(k,:)==pencil{k,j}(n-removed)) || any(pen(:,j)==pencil{k,j}(n-removed))
+                        if any(eliminations==pencil{k,j}(n-removed))
                             pencil{k,j} = pencil{k,j}(pencil{k,j}~=pencil{k,j}(n-removed));
                             removed = removed + 1;
                         end
                     end
                     lengths(k,j) = length(pencil{k,j});
+                    
                 end
+                
             end
+            
+            % check for a finished puzzle
+            if all(all(lengths==1))
+                break
+            end
+            
         end
     end
     
-    solved = all(all(lengths==1));
 end
 
+% check validity of puzzle solution
 if checkpuzzle(pen)
     solved = pen;
 else
